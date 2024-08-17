0e808a45c254561f022a6344bfa1933c8e6a89a7
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-08-17 19:59:02 +0200

    components created

--- portafolioReact/portafolioReact/src/App.jsx
+++ portafolioReact/portafolioReact/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "../src/components/Header";
+import About from "../src/components/About";
+import Projects from "../src/components/Projects";
+import Contact from "../src/components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +36,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
